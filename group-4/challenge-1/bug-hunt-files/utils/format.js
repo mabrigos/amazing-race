8c26c8416365d7d095d206a1992c2ca30a709771
Timothy Ryeyn Perez <timothy.perez@cambridge.org>
2025-10-08 12:28:32 +0800

    added changes on challenge 1 hehe sry

--- group-4/challenge-1/bug-hunt-files/utils/format.js
+++ group-4/challenge-1/bug-hunt-files/utils/format.js
@@ -2,11 +2,11 @@
 // This should format a todo into { id, name, done }
 
 function formatTodo(todo) {
-  return {
-    id: todo.id, // ✅ Corrected property
-    name: todo.title, // ✅ Corrected property
-    done: todo.completed // ✅ Corrected property
-  };
+	return {
+		id: todo.id, // ✅ Corrected property
+		name: todo.title, // ✅ Corrected property
+		done: todo.completed // ✅ Corrected property
+	}
 }
 
-module.exports = { formatTodo };
+module.exports = { formatTodo }
